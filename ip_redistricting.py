df8a5c58b78691958a41acf56b79dac6629ff2ac
Leslie <lesliekanderson@fastmail.com>
2024-02-09 13:15:11 -0500

    start building model

--- ip_redistricting.py
+++ ip_redistricting.py
@@ -1,6 +1,6 @@
 import json
-import requests
-import csv
+import numpy as np
+from pulp import LpProblem, LpMinimize, LpVariable, lpSum
 
 # load NJ counties and adjacent counties into a dict
 njc_file = 'data/nj_counties.json'
@@ -39,6 +39,26 @@ print('Updated total population: ', model_total_pop)
 print('Updated number of districts: ', model_n_districts)
 print('Updated desired population: ', model_total_pop/model_n_districts)
 
-        
-        
+#### THE MODEL ####
+model = LpProblem("Redistricting", LpMinimize)
+variable_names = [str(i)+str(j) for j in range(1, model_n_districts+1) for i in range(1, model_n_counties+1)]
+variable_names.sort() 
+#print(variable_names)
 
+# The Decision Variable is 1 if the county is assigned to district k.
+DV_variable_d = LpVariable.matrix("d", variable_names, cat="Binary", lowBound=0, upBound=1)
+d_assignment = np.array(DV_variable_d).reshape(model_n_counties, model_n_districts)   
+#print(assignment)       
+
+# Objective Function
+
+# Constraints
+# Add constraint that each county is assigned to only one district
+# Not 100% sure this is correct
+for i in range(model_n_counties):
+    model += lpSum(d_assignment[i][j] for j in range(model_n_counties)) == 1
+    
+# Add constraint that each district is within desired population range
+# Try min 90% and max 110%
+
+# Add continuity constraints
